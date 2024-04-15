b6b6e4b18c048b52263a899215644db99bb0e20d
ZanyFox <2750668305@qq.com>
2024-04-15 10:32:06 +0800

    完善功能

---
+++ src/views/dir/menu1/index.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+  <h3>我是menu1</h3>
+</template>
+
+<style scoped lang="scss">
+
+</style>
---
+++ src/views/dir/menu2/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h3>我是menu2</h3>
+    <!--  <router-view/>-->
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
